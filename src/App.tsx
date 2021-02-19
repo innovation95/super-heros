5715dee31058e0529d9fd7a64efb2e234529e03c
Vipin Sharma <it.vipin.sharma@hotmail.com>
2021-02-19 22:25:22 +0530

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          super heroes play
+          super heroes players
         </p>
         <a
           className="App-link"
