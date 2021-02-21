b2ae3b322f8282aa8e7f1a3fa0b97cffe30f43dd
Vipin sharma <innovative2021project@outlook.com>
2021-02-21 22:51:56 +0530

    Added scss support

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './assets/styles/style.scss';
 import Home from './components/home/home.component';
 
 function App() {
